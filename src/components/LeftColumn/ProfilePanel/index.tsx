338140e198fbe50eef77ef2de398bee51f5c2df9
unknown <thiago.fidelis27@gmail.com>
2020-09-28 21:02:59 -0300

    add clone-linkedin with shimmer effect

---
+++ src/components/LeftColumn/ProfilePanel/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import Panel from '../../Panel';
+
+import { Container } from './styles';
+
+const ProfilePanel: React.FC = () => {
+  return (
+    <Panel>
+      <Container>
+        <div className="profile-cover"></div>
+        <img
+          src="https://github.com/fidelis27.png"
+          alt="Avatar"
+          className="profile-picture"
+        />
+        <h1>Thiago Fidelis</h1>
+        <h2>Desenvolvedor FullStack </h2>
+        <h2>ReactJS/NodeJS</h2>
+
+        <div className="separator"></div>
+
+        <div className="key-value">
+          <span className="key">Quem viu seu perfil</span>
+          <span className="value">1.558</span>
+        </div>
+        <div className="key-value">
+          <span className="key">Viram sua publicação</span>
+          <span className="value">388</span>
+        </div>
+      </Container>
+    </Panel>
+  );
+};
+
+export default ProfilePanel;
